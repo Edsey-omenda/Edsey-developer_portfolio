dbaa9590840d853258a4f6f4e25e46aef179948e
Edsey omenda <omendajames0317@gmail.com>
2022-06-04 16:02:04 +0300

    adds function that modifies the chat form on the web page

---
+++ index.js
@@ -0,0 +1,7 @@
+function openChat() {
+    document.getElementById("letChat").style.display = "block";
+  }
+  
+  function closeChat() {
+    document.getElementById("letChat").style.display = "none";
+  }
